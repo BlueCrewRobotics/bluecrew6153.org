b5745faf955685153e43677ce42458392ce56854
Matt Gallant <chillinerd@gmail.com>
2018-03-13 20:30:55 -0400

    Add Kylo to Robots Page

--- robots/index.php
+++ robots/index.php
@@ -24,25 +24,35 @@ else {
 <h1>Robots</h1>
 
 <div class="media">
-<div class="media-left">
-    <img class="media-object" src="../resources/images/bot2017.png" width="140" height="140">
-</div>
-<div class="media-body">
-    <h4 class="media-heading">BluCubed</h4>
-    <h5>2017: <a href="https://www.firstinspires.org/sites/default/files/uploads/resource_library/frc/game-and-season-info/competition-manual/2017/FIRST-FRC-SpectatorFlyer'17-FR021-FORM.pdf" target="_blank">FIRST Steamworks</a></h5>
-    <p>BluCubed was named after it's somewhat cubed shape and design. It was originally developed to shoot "fuel" into the "boiler", but later changed to primarily deliver gears and to climb as shown in the picture to the left.</p>
-</div>
+    <div class="media-left">
+        <img class="media-object" src="../resources/images/bot2018.png" width="140" height="140">
+    </div>
+    <div class="media-body">
+        <h4 class="media-heading">Kylo</h4>
+        <h5>2018: <a href="https://www.firstinspires.org/sites/default/files/uploads/resource_library/frc/game-and-season-info/competition-manual/2018/first-spectatorflyer18-frc-ltr-dec-form.pdf" target="_blank">FIRST Power Up</a></h5>
+        <p>Kylo was named after Kylo Ren because of the wings that it uses to push up other robots in the end of every match. In addition to these wings, it also have a elevator mechanism that allows the cube shooting mechanism to move up and down.</p>
+    </div>
 </div>
 <div class="media">
-<div class="media-left">
-    <img class="media-object" src="../resources/images/bot2016.png" width="140" height="140">
+    <div class="media-left">
+        <img class="media-object" src="../resources/images/bot2017.png" width="140" height="140">
+    </div>
+    <div class="media-body">
+        <h4 class="media-heading">BluCubed</h4>
+        <h5>2017: <a href="https://www.firstinspires.org/sites/default/files/uploads/resource_library/frc/game-and-season-info/competition-manual/2017/FIRST-FRC-SpectatorFlyer'17-FR021-FORM.pdf" target="_blank">FIRST Steamworks</a></h5>
+        <p>BluCubed was named after it's somewhat cubed shape and design. It was originally developed to shoot "fuel" into the "boiler", but later changed to primarily deliver gears and to climb as shown in the picture to the left.</p>
+    </div>
 </div>
-<div class="media-body">
-    <h4 class="media-heading">Concussion Protocol</h4>
-    <h5>2016: <a href="https://www.firstinspires.org/sites/default/files/uploads/resource_library/frc/game-and-season-info/competition-manual/2016/first-stronghold-game-onepage.pdf" target="_blank">FIRST Stronghold</a></h5>
-    <p>Concussion Protocol was aptly named after several brown outs, or "concussions", where our radio cut out leaving us stranded mid-game. This robot was built to mainly tackle obstacles and shoot "boulders" if needed.</p>
-</div>
-<br />
+<div class="media">
+    <div class="media-left">
+        <img class="media-object" src="../resources/images/bot2016.png" width="140" height="140">
+    </div>
+    <div class="media-body">
+        <h4 class="media-heading">Concussion Protocol</h4>
+        <h5>2016: <a href="https://www.firstinspires.org/sites/default/files/uploads/resource_library/frc/game-and-season-info/competition-manual/2016/first-stronghold-game-onepage.pdf" target="_blank">FIRST Stronghold</a></h5>
+        <p>Concussion Protocol was aptly named after several brown outs, or "concussions", where our radio cut out leaving us stranded mid-game. This robot was built to mainly tackle obstacles and shoot "boulders" if needed.</p>
+    </div>
+    <br />
 </div>
 </div>
 
